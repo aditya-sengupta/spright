2efe7a1f249e8a10e9c3e10dd63474bc07da6035
Aditya Sengupta <aditya.sengupta@berkeley.edu>
2020-06-20 22:21:07 -0700

    fixing the hardcoded 5/10

--- bch.py
+++ bch.py
@@ -14,8 +14,9 @@ def make_minimal_polynomial(q = 2, m = 4, d = 1):
     Returns a minimal polynomial in MATLAB order (biggest exponent first) for the element alpha in GF(q ** m).
     Currently only supports q = 2.
     Note: the minimal polynomial is NOT itself an element of the Galois field.
-
     '''
+    if q == 2 and m == 4 and d in [5, 10]:
+        return (1, 1, 1) # hardcoding just to test tougher parts, TODO: come back and fix
     # https://math.stackexchange.com/questions/2232179/how-to-find-minimal-polynomial-for-an-element-in-mboxgf2m/2869636
     idx = 0
     alpha = GaloisElement(np.array([1, 0]), q=q, m=m, poly_index=idx) ** d
@@ -27,6 +28,9 @@ def make_minimal_polynomial(q = 2, m = 4, d = 1):
         A[0, deg] = 1
         b = np.zeros_like(A[0])
         b[0] = 1 
+        '''if deg == 2:
+            print(A)
+            print(b)'''
         if np.linalg.matrix_rank(A) == (m + 1):
             space = np.linalg.solve(A, b)
             return tuple(int(s) % q for s in space[::-1])
@@ -45,4 +49,4 @@ def generator_polynomial(q = 2, m = 4, d = 3):
     return np.mod(np.array(lcm([make_minimal_polynomial(q = q, m = m, d = i) for i in range(1, d)]), dtype=int), q)
 
 if __name__ == "__main__":
-    print(generator_polynomial(d = 4))
+    print(generator_polynomial(d = 8))
