debabefb7b307f75a3bfa1cb9aad5fb6ba061577
Aditya Sengupta <aditya.sengupta@berkeley.edu>
2020-06-20 22:35:11 -0700

    formally given up on q^m = 16, d = 5 or 10

--- bch.py
+++ bch.py
@@ -6,6 +6,7 @@ import numpy as np
 from scipy import linalg
 from functools import reduce
 import warnings
+import copy
 
 from galois import GaloisElement
 
@@ -16,7 +17,7 @@ def make_minimal_polynomial(q = 2, m = 4, d = 1):
     Note: the minimal polynomial is NOT itself an element of the Galois field.
     '''
     if q == 2 and m == 4 and d in [5, 10]:
-        return (1, 1, 1) # hardcoding just to test tougher parts, TODO: come back and fix
+       return (1, 1, 1) # hardcoding just to test tougher parts, TODO: come back and fix
     # https://math.stackexchange.com/questions/2232179/how-to-find-minimal-polynomial-for-an-element-in-mboxgf2m/2869636
     idx = 0
     alpha = GaloisElement(np.array([1, 0]), q=q, m=m, poly_index=idx) ** d
@@ -24,18 +25,18 @@ def make_minimal_polynomial(q = 2, m = 4, d = 1):
     if q != 2:
         warnings.warn("tbd how to vary parameters for q > 2")
     deg = m
+    start_A = copy.deepcopy(A)
     while deg > 0:
-        A[0, deg] = 1
+        zero_inds = np.where(~A.any(1))[0]
         b = np.zeros_like(A[0])
-        b[0] = 1 
-        '''if deg == 2:
-            print(A)
-            print(b)'''
-        if np.linalg.matrix_rank(A) == (m + 1):
+        for i, ind in enumerate(zero_inds):
+            A[i, deg - ind] = 1
+            b[i] = 1
+        try:
             space = np.linalg.solve(A, b)
             return tuple(int(s) % q for s in space[::-1])
-        else:
-            A[0, deg] = 0
+        except np.linalg.LinAlgError:
+            A = start_A
             deg -= 1
     raise NotImplementedError("should never hit this")
 
@@ -49,4 +50,6 @@ def generator_polynomial(q = 2, m = 4, d = 3):
     return np.mod(np.array(lcm([make_minimal_polynomial(q = q, m = m, d = i) for i in range(1, d)]), dtype=int), q)
 
 if __name__ == "__main__":
-    print(generator_polynomial(d = 8))
+    for i in range(2, 15):
+        print(make_minimal_polynomial(d = i))
+        print(generator_polynomial(d = i))
